6d7e5532d8cd61aa52a3551539551f5637aa307a
Alice Freda <alicefreda@gmail.com>
2019-11-23 22:23:18 +0000

    simplify CLI options

--- lib/ghibli_db/cli.rb
+++ lib/ghibli_db/cli.rb
@@ -2,42 +2,48 @@ class GhibliDb::CLI
 
   def start
     welcome
-    menu
+    list_films
+    options
   end
 
   def welcome
     puts "Welcome to the Studio Ghibli Database!"
+    puts "--------------------------------------"
   end
 
-  def menu
-    # add .gsub /^\s*/, '' below to keep the indentation from the Here DOC from displaying in the CLI
-    puts <<-DOC
-      ------------------------------------------------------------------
-      Please choose an option number or type "exit" to exit the program:
-      ------------------------------------------------------------------
-      1. I would like to see a list of all the Studio Ghibli films
-      2. I would like to search for a Studio Ghibli film
-      ------------------------------------------------------------------
-    DOC
-      # 3. I would like a to see a list of species included in the films
-      # 4. I would like to search the films by title
-      # 5. I would like to search the films by species
-      # 6. I would like some trivia / facts / a quiz
-      main_pick
-  end
+  # def menu
+  #   # add .gsub /^\s*/, '' below to keep the indentation from the Here DOC from displaying in the CLI
+  #   # puts <<-DOC
+  #   #   ------------------------------------------------------------------
+  #   #   Please choose an option number or type "exit" to exit the program:
+  #   #   ------------------------------------------------------------------
+  #   #   1. I would like to see a list of all the Studio Ghibli films
+  #   #   2. I would like to search for a Studio Ghibli film
+  #   #   ------------------------------------------------------------------
+  #   # DOC
+  #   #   # 3. I would like a to see a list of species included in the films
+  #   #   # 4. I would like to search the films by title
+  #   #   # 5. I would like to search the films by species
+  #   #   # 6. I would like some trivia / facts / a quiz
+  #     main_pick
+  # end
 
-  def main_pick
-    GhibliDb::API.get_films
+  def options
     input = gets.strip
-    if input == "1"
-      list_films
-    end
   end
+
+  # def main_pick
+  #   GhibliDb::API.get_films
+  #   input = gets.strip
+  #   if input == "1"
+  #     list_films
+  #   end
+  # end
 
   def list_films
+    GhibliDb::API.get_films
     GhibliDb::Film.all.each.with_index(1) do |film, index|
       puts "#{index}. #{film.title}"
-      puts "-----------------------------------------------------------------------------------"
     end
     puts ""
     puts "please enter the number corresponding to a movie you would like more information on"
