c9d9a56b22b9ecd543be41f2d1b8fd9bb1c06678
Alice Freda <alicefreda@gmail.com>
2019-11-23 18:55:02 +0000

    add missing end

--- lib/ghibli_db/film.rb
+++ lib/ghibli_db/film.rb
@@ -5,8 +5,8 @@ class GhibliDb::Film
 
 @@all = []
 
-def initialize(attributes) #mass assignment
-  attributes.each do |key, value| # this will iterate and assign attributes to each new Film based the attr_accesors set above
+def initialize(attributes) #True? this will work for single films as well as collections of movies based on structure of the film array. ??
+  attributes.each do |key, value| # this will iterate and assign attributes to a new Film based the attr_accesors set above
     self.send("#{key}=", value) if self.respond_to?(key) #checks to see if that object has a method that it can call for that key (a getter/reader method). if there is no reader for that key, it will not execute the setter for that.
   end
 end
@@ -19,21 +19,22 @@ def self.all
   @@all
 end
 
-def self.create_from_collection(attributes)
-  new_film = GhibliDb::Film.new(attributes)
+def self.create_from_collection(collection)
+  new_film = GhibliDb::Film.new(collection)
   new_film.save
 end
 
-# def self.find_by_id(id)
-#   self.all.detect {|film| film.id == id}
+# def self.find_by_id(title)
+#   self.all.detect {|film| film.title == title}
 # end
-# 
+#
 # def self.create_by_id(id)
 #   self.new(id).tap do |film|
 #       film.save
 #     end
 # end
-# 
+#
 # def self.find_or_create_by_id(id)
 #   find_by_id(id) || self.create_by_id(id)
 # end
+end
