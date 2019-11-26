f48f6642e63198476b588fd0b9f16ae33b196e7c
Alice Freda <alicefreda@gmail.com>
2019-11-26 00:23:22 +0000

    fix typo

--- lib/ghibli_db/film.rb
+++ lib/ghibli_db/film.rb
@@ -1,14 +1,14 @@
 class GhibliDb::Film
   #what parts of this could come from a module if we decide to make a species class?
 
-  attr_accessor :id, :title, :description, :release_date, :species, :locations, :url, :people
+  attr_accessor :id, :title, :description, :release_date, :url, :species, :locations, :people
 
   @@all = []
 
   def initialize(attributes)
     binding.pry
     attributes.each do |key, value| # this will iterate and assign attributes to a new Film based the attr_accesors set above
-      self.send("#{key}=", value) if self.respond_to?(key) && (key != #checks to see if that object has a method that it can call for that key (a getter/reader method). if there is no reader for that key, it will not execute the setter for that.
+      self.send("#{key}=", value) if self.respond_to?(key) #checks to see if that object has a method that it can call for that key (a getter/reader method). if there is no reader for that key, it will not execute the setter for that.
 ####### if species, locations, or people equals https://ghibliapi.herokuapp.com/species/, https://ghibliapi.herokuapp.com/locations/, or https://ghibliapi.herokuapp.com/people/ then change that to "n/a"
     end
   end
