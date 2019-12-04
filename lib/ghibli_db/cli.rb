a8320b21cf935d3f3fe033339d91ad87a98a7cf7
Alice Freda <alicefreda@gmail.com>
2019-12-04 01:46:01 +0000

    add main menu options and character list

--- lib/ghibli_db/cli.rb
+++ lib/ghibli_db/cli.rb
@@ -1,24 +1,66 @@
 class GhibliDb::CLI
 
   def start
+    pastel
     welcome
     GhibliDb::Film.make_films ## this takes a long time to load, figure out how to shorten
-    list_films
-    options
+    main_menu
   end
 
   def welcome
-    pastel
     puts pastel.yellow("    ----------------------------------------------------------------------")
     puts pastel.cyan("    Welcome to the Studio Ghibli Database!")
     puts pastel.yellow("    ----------------------------------------------------------------------")
     sleep(1)
     spirited_img
     line_break
-    puts pastel.red("    Please wait a few moments while I fetch the films...")
+    puts pastel.red("    Please take a deep breath and count to ten while I fetch the films...")
   end
 
-  def options
+  def main_menu
+    puts <<-DOC
+    ------------------------------------------------------------------
+    Choose an option number or type "exit" to exit the program:
+    ------------------------------------------------------------------
+    1. I would like to see a list of all the Studio Ghibli films
+    2. I would like to see a list of some characters from the films
+    ------------------------------------------------------------------
+    DOC
+    # 3. I would like a to see a list of species included in the films
+    # 4. I would like to search the films by title
+    # 5. I would like to search the films by species
+    # 6. I would like to search for a Studio Ghibli film
+    input = gets.strip.to_i
+    if input == 1
+      list_films
+    else
+      list_characters
+    end
+  end
+
+  def list_films
+    line_break
+    puts pastel.cyan("     #") + " | " + pastel.cyan("Title                            ")
+    puts "    ---|----------------------------------"
+    GhibliDb::Film.all_sorted.each.with_index(1) do |film, index|
+      puts "    #{index}  | #{film.title}" if index < 10
+      puts "    #{index} | #{film.title}" if index > 9
+      sleep(0.25)
+    end
+    film_details
+  end
+
+  def list_characters
+    puts pastel.cyan("     #") + " | " + pastel.cyan("Character                            ")
+    puts "    ---|----------------------------------"
+      GhibliDb::Person.all.each.with_index(1) do |person, index|
+        puts "    #{index}  | #{person.name}, a #{person.species}" if index < 10
+        puts "    #{index} | #{person.name}, a #{person.species}" if index > 9
+        sleep(0.25)
+      end
+  end
+
+  def film_details
     line_break
     pastel
     puts pastel.red("Enter the number of the movie you would like more information on:")
@@ -45,15 +87,6 @@ class GhibliDb::CLI
       puts pastel.yellow("------------------------------")
       input.species.each.with_index(1) { |species, index| puts "#{index}. #{species.name}"}
       line_break
-    end
-  end
-
-  def list_films
-    puts "    ---|----------------------------------"
-    GhibliDb::Film.all_sorted.each.with_index(1) do |film, index|
-      puts "    #{index}  | #{film.title}" if index < 10
-      puts "    #{index} | #{film.title}" if index > 9
-      sleep(0.25)
     end
   end
 
