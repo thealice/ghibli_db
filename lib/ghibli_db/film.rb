882d712a2d478fed3e0ac15e265d3a8757dd5c31
Alice Freda <alicefreda@gmail.com>
2019-12-06 21:06:27 +0000

    return the accidentally deleted film class

--- lib/ghibli_db/film.rb
+++ lib/ghibli_db/film.rb
@@ -1,7 +1,7 @@
 class GhibliDb::Film
   #what parts of this could come from a module if we decide to make a species class?
 
-  attr_accessor :id, :title, :description, :release_date, :url, :locations, :people, :species, :director
+  attr_accessor :id, :title, :description, :release_date, :url, :locations, :species, :director, :people
 
   @@all = []
 
@@ -10,13 +10,14 @@ class GhibliDb::Film
     attributes.each do |key, value| # this will iterate and assign attributes to a new Film based the attr_accesors set above
       self.send("#{key}=", value) if self.respond_to?(key) #checks to see if that object has a method that it can call for that key (a getter/reader method). if there is no reader for that key, it will not execute the setter for that.
     end
-    @people = nil if self.people == ["https://ghibliapi.herokuapp.com/people/"]
+    # @people = nil if self.people == ["https://ghibliapi.herokuapp.com/people/"]
     @locations = nil if self.locations == ["https://ghibliapi.herokuapp.com/locations/"]
     @species = nil if self.species == ["https://ghibliapi.herokuapp.com/species/"]
-    @num = nil
+    # @num = nil
     self.add_people
-    self.add_species
-    # self.add_list_num
+
+    # self.add_species
+    # # self.add_list_num
   end
 
   def save
@@ -92,7 +93,8 @@ class GhibliDb::Film
   end
 
   def self.create_by_url(url)
-    self.new(url).tap do |object|
+    object_hash = GhibliDb::API.get_object_by_url(url)
+    self.new(object_hash).tap do |object|
         object.save
       end
   end
